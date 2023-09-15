b89891bca94311e75b3d1cd3eeda6b9936ef86a2
Rassili <adnan.rassili47@gmail.com>
2023-09-15 17:19:48 +0100

    seconde commit

---
+++ challenge15.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+      int moyenne;
+      printf ("entre votre moyenne:\n");
+      scanf(" %d", &moyenne);
+      if(moyenne<10){
+         printf("recalé");
+                   }else if(moyenne>=10 && moyenne<12 ){
+                      printf("passable");
+                            }else if(moyenne>=12 && moyenne<14 ){
+                                 printf("assez bien");
+                                     }else if(moyenne>=14 && moyenne<16 ){
+                                            printf("bien");
+     }else{
+         printf("très bien");
+      }
+ return 0;
+}
